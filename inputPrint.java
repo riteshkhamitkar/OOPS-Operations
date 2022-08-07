6bfbdf531d00fb0aa0954bc1c6fa44211a00d66d
riteshkhamitkar <89683381+riteshkhamitkar@users.noreply.github.com>
2022-08-07 17:42:38 +0530

    Create inputPrint.java

---
+++ inputPrint.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter Username:");
+        
+        String username = myObj.nextLine();
+        System.out.println("Hello boi: " + username);
+    }
+}
