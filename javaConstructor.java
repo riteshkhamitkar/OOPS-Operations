fc02df673854e259fdd7980b780e07e6dc55dd74
riteshkhamitkar <89683381+riteshkhamitkar@users.noreply.github.com>
2022-08-07 18:16:28 +0530

    Create javaConstructor.java

---
+++ javaConstructor.java
@@ -0,0 +1,12 @@
+public class Main{
+    int x;
+    
+    public Main(){
+        x=6;
+    }
+    public static void main(String[] args){
+        Main ritesh = new Main();
+        System.out.println(ritesh.x);
+        
+    }
+}
