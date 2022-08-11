ad7cc180eca1800c2bad4b0490ec42c348e74e75
riteshkhamitkar <89683381+riteshkhamitkar@users.noreply.github.com>
2022-08-11 13:14:47 +0530

    Create AddinJava.java

---
+++ AddinJava.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner ritesh = new Scanner(System.in);
+        System.out.println("Enter");
+        
+        double n1,n2,n3;
+        n1 = ritesh.nextInt();
+        n2 = ritesh.nextInt();
+        n3 = n1+n2;
+        System.out.println("Sum is "+n3);
+    }
+}
